d3c0e0baf7a439a79505cc93baf900dc09a67388
Sehajpreet Singh <ghotrasehaj03@gmail.com>
2026-06-30 11:50:25 +0530

    fix dashbord colorr

--- src/ui/base_layout.py
+++ src/ui/base_layout.py
@@ -212,7 +212,7 @@ def style_background_dashboard():
         """
 <style>
 .stApp {
-    background: #FFB6C1 !important;
+    background: #E0E3FF !important;
 }
 /* Style text/headers for dashboard page to be dark navy for perfect readability */
 .stApp h1, .stApp h2, .stApp h3, .stApp h4 {
