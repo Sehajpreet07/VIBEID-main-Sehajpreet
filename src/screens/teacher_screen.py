4a8b30c3617dde746dfd6158a78024f11a3c203c
Sehajpreet Singh <ghotrasehaj03@gmail.com>
2026-06-30 11:14:52 +0530

    Fix manage subjects

--- src/screens/teacher_screen.py
+++ src/screens/teacher_screen.py
@@ -5,7 +5,7 @@ from src.ui.base_layout import style_background_dashboard, style_base_layout
 from src.components.snapheader import header_dashboard
 from src.components.footer import footer_dashboard
 from src.components.subject_card import subject_card
-from src.database.db import check_teacher_exists, create_teacher, teacher_login, get_teacher_subjects, get_attendance_for_teacher
+from src.database.db import check_teacher_exists, create_teacher, teacher_login, get_teacher_subjects, get_attendance_for_teacher,get_subject_students
 from src.components.dialog_create_subject import create_subject_dialog
 from src.components.dialog_share_subject import share_subject_dialog
 from src.components.dialog_add_photo import add_photos_dialog
@@ -13,10 +13,7 @@ from src.components.dialog_add_photo import add_photos_dialog
 from src.pipelines.face_pipeline import predict_attendance
 from src.components.dialog_attendance_results import attendance_result_dialog
 import numpy as np
-from src.database.db import (
-    get_teacher_subjects,
-    get_subject_students,
-)
+ 
 from datetime import datetime
 
 import pandas as pd
